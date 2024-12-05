b0730cb23992294b2995aad6bd64b1d271c7251d
Patrick Knowles <pgknowles1@gmail.com>
2024-12-04 22:29:28 -0600

    silverstone race

--- F124_season.py
+++ F124_season.py
@@ -8,6 +8,7 @@ from PIL import Image
 # Load the data
 df = pd.read_excel('F124_season.xlsx', sheet_name='PythonReadyData')
 
+
 race_points = ['SuzukaPoints','SilverstonePoints','AustraliaPoints', 
                 'SpaPoints','SpainPoints','ChinaSprintPoints','ChinaPoints', 
                 'BakuPoints','CanadaPoints','MonzaPoints','Abu DhabiPoints', 
